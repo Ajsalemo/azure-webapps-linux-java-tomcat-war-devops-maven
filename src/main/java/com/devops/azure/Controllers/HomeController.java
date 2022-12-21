daa9341d5c5aa46d9e4d1dfb7ca841f1d029f684
Anthony Salemo <ajssalemo@gmail.com>
2022-12-21 18:43:23 -0500

    updating to use az cli as the deployment task

--- src/main/java/com/devops/azure/Controllers/HomeController.java
+++ src/main/java/com/devops/azure/Controllers/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
-    String message = "This is a war file from GitHub Actions with Maven!";
+    String message = "This is a war file from GitHub Actions with Maven and the AZ CLI!";
 
     @GetMapping("/")
     public String index() {
